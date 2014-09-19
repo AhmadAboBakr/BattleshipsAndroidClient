d60a18ceb51dcd155b9a26fba40e0c228a990a5e
Joe Nofal <joe.nofal@gmail.com>
2014-09-19 17:18:25 +0200

    main game

--- java/com/basratec/battleships/MainGame.java
+++ java/com/basratec/battleships/MainGame.java
@@ -1,164 +1,81 @@
 package com.basratec.battleships;
 
-import com.basratec.battleships.util.SystemUiHider;
+import java.util.ArrayList;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.os.Build;
+import android.R.bool;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
 import android.view.View;
-import com.basratec.battleships.R;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
 public class MainGame extends Activity {
-    SocketSinglton connection;
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
+	private ArrayList<String> ARRV = new ArrayList<String>(25);
+	public int[] gridMap = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	private int CountArr = 0;
+	private boolean FLAG = true;
+	// custom drawing view
+//	private DrawingView drawView;
+	// paint color button in the palette
+	private ImageButton currPaint;
 
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game);
 
-    /**
-     * If set, will toggle the system UI visibility upon interaction. Otherwise,
-     * will show the system UI visibility upon interaction.
-     */
-    private static final boolean TOGGLE_ON_CLICK = true;
+		// get drawing view
+		// instantiate this variable by retrieving a reference to it from the
+		// layout
+		// drawView = (DrawingView) findViewById(R.id.drawing);
+		/*
+		 * first paint color button First retrieve the Linear Layout it is
+		 * contained in..
+		 */
+		LinearLayout paintLayout = (LinearLayout) findViewById(R.id.paint_colors);
+		// first button and store it as the instance variable
+		// currPaint = (ImageButton)paintLayout.getChildAt(0);
+		// currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+	}
 
-    /**
-     * The flags to pass to {@link SystemUiHider#getInstance}.
-     */
-    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		//getMenuInflater().inflate(R.menu., menu);
+		return true;
+	}
 
-    /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
-    private SystemUiHider mSystemUiHider;
+	// usr clicked color
+	public void paintClicked(View view) {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        try {
-            connection = SocketSinglton.getInstance();
-        }
-        catch(Exception e){}
-        setContentView(R.layout.activity_game);
+		// if(view != currPaint){
+		// new color
+		// retrieve the tag of button
+		ImageButton imgView = (ImageButton) view;
+		String color = view.getTag().toString();
+		int x = Integer.parseInt(color);
+		if (gridMap[x] != 1) {
+			gridMap[x] = 1;
 
-        final View controlsView = findViewById(R.id.fullscreen_content_controls);
-        final View contentView = findViewById(R.id.fullscreen_content);
+			// drawView.setColor(color);
+			Log.d("youssef", color);
+			Log.d("fffffffff", "ffffffffff");
+			// update UI
+			if (x == 14) {// if he hit ship red color appears
+				imgView.setImageDrawable(getResources().getDrawable(
+						R.drawable.paint_hit));
 
-        // Set up an instance of SystemUiHider to control the system UI for
-        // this activity.
-        mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
-        mSystemUiHider.setup();
-        mSystemUiHider
-                .setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-                    // Cached values.
-                    int mControlsHeight;
-                    int mShortAnimTime;
+			} else
+				imgView.setImageDrawable(getResources().getDrawable(
+						R.drawable.paint_pressed));
 
-                    @Override
-                    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-                    public void onVisibilityChange(boolean visible) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-                            // If the ViewPropertyAnimator API is available
-                            // (Honeycomb MR2 and later), use it to animate the
-                            // in-layout UI controls at the bottom of the
-                            // screen.
-                            if (mControlsHeight == 0) {
-                                mControlsHeight = controlsView.getHeight();
-                            }
-                            if (mShortAnimTime == 0) {
-                                mShortAnimTime = getResources().getInteger(
-                                        android.R.integer.config_shortAnimTime);
-                            }
-                            controlsView.animate()
-                                    .translationY(visible ? 0 : mControlsHeight)
-                                    .setDuration(mShortAnimTime);
-                        } else {
-                            // If the ViewPropertyAnimator APIs aren't
-                            // available, simply show or hide the in-layout UI
-                            // controls.
-                            controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
-                        }
+			// currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+			// currPaint = (ImageButton) view;
+			// }
+		}
 
-                        if (visible && AUTO_HIDE) {
-                            // Schedule a hide().
-                            delayedHide(AUTO_HIDE_DELAY_MILLIS);
-                        }
-                    }
-                });
-
-        // Set up the user interaction to manually show or hide the system UI.
-        contentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (TOGGLE_ON_CLICK) {
-                    mSystemUiHider.toggle();
-                } else {
-                    mSystemUiHider.show();
-                }
-            }
-        });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
-    }
-
-
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
-
-    Handler mHideHandler = new Handler();
-    Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            mSystemUiHider.hide();
-        }
-    };
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
-    }
+	}
 }
