0433d1ff0c70bc8e6657962e9a5ec1928cd1a1a7
ahmed maged <ahmaadmaged@gmail.com>
2014-09-27 17:44:47 +0200

    fixes #6 .Added the MainGame activity

---
+++ java/com/basratec/battleships/MainGame.java
@@ -0,0 +1,15 @@
+package com.basratec.battleships;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by nookz on 9/27/2014.
+ */
+public class MainGame extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
