d502a251f5fe9f68b5247d7d7bc1e0e78099df41
Joe Nofal <joe.nofal@gmail.com>
2014-09-29 13:30:28 +0200

    3 files: 1)MG.java(concept only NOT compleated)  2)MG.xml 3)dimens.xml

--- java/com/basratec/battleships/MainGame.java
+++ java/com/basratec/battleships/MainGame.java
@@ -2,14 +2,39 @@ package com.basratec.battleships;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 /**
  * Created by nookz on 9/27/2014.
+ * 
+ * @param <boolen>
  */
-public class MainGame extends Activity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
+public class MainGame extends Activity {
+
+	private boolean PLAY_FLAG;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_game);
+	}
+// not working yet ..
+	private void intializeMyGrid(int [] MyGrid) {
+/*
+		LinearLayout MyGridLayout = (LinearLayout) findViewById(R.id.MyGrid);
+		for (int i = 0; i <= gridSize; i++) {
+			if (MyGrid[i] == 1) {
+				currCell = (ImageButton) MyGridLayout.getChildAt(i);
+				currCell.setImageDrawable(getResources().getDrawable(
+						R.drawable.ship));
+			}
+		}*///endFor
+	}//endmethod
+
+	public boolean play() {
+		PLAY_FLAG = true;
+		// don't forget to disable it after play
+		return PLAY_FLAG;
+	}
 }
