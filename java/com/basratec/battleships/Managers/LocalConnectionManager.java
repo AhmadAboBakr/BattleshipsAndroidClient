f111ba26fb65d0c9ed226807610aa3c32cb36eec
ahmed maged <ahmaadmaged@gmail.com>
2014-10-13 23:42:17 +0200

    fixed compile error due to missing function implementations

--- java/com/basratec/battleships/Managers/LocalConnectionManager.java
+++ java/com/basratec/battleships/Managers/LocalConnectionManager.java
@@ -1,5 +1,9 @@
 package com.basratec.battleships.Managers;
 
+import com.basratec.battleships.AAPIableActivity;
+
+import java.util.concurrent.Callable;
+
 /**
  * Created by bakr on 10/10/14.
  */
@@ -14,6 +18,16 @@ public class LocalConnectionManager extends ConnectionManager{
 
     @Override
     public void send(String Message) {
+
+    }
+
+    @Override
+    public void init(Callable successCallBack, Callable failureCallBack) {
+
+    }
+
+    @Override
+    public void startListning(AAPIableActivity activity) {
 
     }
 }
